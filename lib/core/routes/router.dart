c9079c7a5c392f31803854c255ee130bcfbd4632
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-07-27 10:55:52 +0300

    feat: create main screen

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:yummify/core/routes/routes.dart';
 import 'package:yummify/features/auth/presentation/screens/login_screen.dart';
 import 'package:yummify/features/auth/presentation/screens/register_screen.dart';
-import 'package:yummify/features/home/presentation/home.dart';
+import 'package:yummify/features/main/presentation/main_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
-  Routes.home: (context) => const HomeScreen(),
+  Routes.main: (context) => const MainScreen(),
   Routes.login: (context) => const LoginScreen(),
   Routes.register: (context) => const RegistrationScreen(),
 };
