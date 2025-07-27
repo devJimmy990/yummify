ca972c485a1fc939bbfe9649928770d75b3d3810
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-07-27 11:04:24 +0300

    update routes and router

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -3,9 +3,13 @@ import 'package:yummify/core/routes/routes.dart';
 import 'package:yummify/features/auth/presentation/screens/login_screen.dart';
 import 'package:yummify/features/auth/presentation/screens/register_screen.dart';
 import 'package:yummify/features/main/presentation/main_screen.dart';
+import 'package:yummify/features/shopping/presentation/screens/all_meals_screen.dart';
+import 'package:yummify/features/shopping/presentation/screens/shopping_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   Routes.main: (context) => const MainScreen(),
   Routes.login: (context) => const LoginScreen(),
+  Routes.shopping: (context) => const ShoppingScreen(),
+  Routes.allMeals: (context) => const AllMealsScreen(),
   Routes.register: (context) => const RegistrationScreen(),
 };
--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,8 +1,10 @@
 class Routes {
   static const String main = "main";
-  static const String cart = "cart";
   static const String login = "login";
-  static const String profile = "profile";
+  static const String shopping = "shopping";
+  static const String allMeals = "all-meals";
   static const String register = "register";
+  static const String profile = "profile";
+  static const String favorite = "favorite";
   static const String onboarding = "onboarding";
 }
